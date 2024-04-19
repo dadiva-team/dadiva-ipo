8c06d4ac08a5dffbf2e3a04b251b65881a7d16c8
Francisco Medeiros <a46631@alunos.isel.pt>
2024-04-19 22:52:33 +0100

    changes Form DTO

--- code/DadivaAPI/DadivaAPI/routes/form/models/GetFormOutputModel.cs
+++ code/DadivaAPI/DadivaAPI/routes/form/models/GetFormOutputModel.cs
@@ -1,14 +1,20 @@
+using System.Text.Json.Serialization;
 using DadivaAPI.domain;
 
-namespace DadivaAPI.routes.form.models;
-
-public record GetFormOutputModel(List<QuestionModel> Questions, List<RuleModel> Rules)
+namespace DadivaAPI.routes.form.models
 {
-    public GetFormOutputModel(List<Question> Questions, List<Rule> Rules) :
-        this(
-            Questions.ConvertAll(question => new QuestionModel(question)),
-            Rules.ConvertAll(rule => new RuleModel(rule))
-        )
+    public record GetFormOutputModel
     {
+        [JsonPropertyName("questions")]
+        public List<QuestionModel> Questions { get; set; }
+
+        [JsonPropertyName("rules")]
+        public List<RuleModel> Rules { get; set; }
+
+        public GetFormOutputModel(List<Question> questions, List<Rule> rules)
+        {
+            Questions = questions.ConvertAll(question => new QuestionModel(question));
+            Rules = rules.ConvertAll(rule => new RuleModel(rule));
+        }
     }
-};
+}
